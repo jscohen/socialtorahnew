a1040092b29b2a9a7cb2d7120d5f66f0620e3956
Jon Cohen <jon.cohen08@gmail.com>
2019-02-17 12:48:28 -0500

    Update modules

---
+++ src/frontComponents/Comments.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Comments extends Component {
+  render() {
+    return (
+      <div className="comments">
+          <span>Name</span>
+      </div>
+    );
+  }
+}
+
+export default Comments;
