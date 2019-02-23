be51dab4ac993262089e2c0e126be69774795d1b
Jonathan Cohen <jonathancohen@Jonathans-MacBook.local>
2019-02-23 15:43:28 -0500

    Added config for routes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:5000';
+axios.defaults.headers.post['Content-Type'] =
+'application/x-www-form-urlencoded';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
