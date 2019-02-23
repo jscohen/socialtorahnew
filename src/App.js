27e3307c976c97e7aecd9e79b9ef3e0446d1fdd8
Jonathan Cohen <jonathancohen@Jonathans-MacBook.local>
2019-02-23 14:08:14 -0500

    Added comments to initial files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
-import Comments from 'frontCompomenets/Comments';
+import Comments from './frontComponents/Comments';
+import NavBar from './frontComponents/NavBar';
 
+/** Main App class
+*
+**/
 class App extends Component {
+  /**
+  * Renders the App
+  * @return {void}
+  */
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <NavBar />
           <h1>Social Torah</h1>
           <Comments />
         </header>
--- src/frontComponents/Comments.js
+++ src/frontComponents/Comments.js
@@ -1,11 +1,18 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, {Component} from 'react';
+import '../App.css';
 
+/**
+* Main container class for Comments
+*/
 class Comments extends Component {
+  /**
+  * @return {void}
+  */
   render() {
     return (
       <div className="comments">
-          <span>Name</span>
+        <span>Name</span>
+        <span>Test</span>
       </div>
     );
   }
