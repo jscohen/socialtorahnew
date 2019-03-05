b3e0fb79a318540ec792b00b9e48df9adb6f33ad
Jonathan Cohen <jonathancohen@Jonathans-MacBook.local>
2019-03-05 17:56:42 -0500

    Refactored logout to work, use id, put all logic in one component for now

--- src/frontComponents/Nav/NavBar.js
+++ src/frontComponents/Nav/NavBar.js
@@ -28,7 +28,7 @@ class NavBar extends Component {
       errorMessage: '',
       showWelcome: false,
       showError: false,
-      isLoggedIn: false
+      isLoggedIn: false,
     };
   }
 
@@ -41,6 +41,72 @@ class NavBar extends Component {
     }
   }
 
+  logOut = () => {
+    axios.delete('/user/signOut', {params: {
+        id: localStorage.getItem('UserId'),
+        token: localStorage.getItem('Token')
+      }
+    })
+    .then(() => {
+      localStorage.removeItem('UserName')
+      localStorage.removeItem('Token')
+      localStorage.removeItem('UserId')
+      this.setState({
+        isLoggedIn: false,
+        showWelcome: false
+      })
+    })
+  }
+
+  /** Submits sign up for authentication **/
+  submitSignUp = () => {
+    axios.post('/user/signUp', {
+      email: this.state.email,
+      password: this.state.email
+    })
+    .then((response) => {
+      if (typeof response.data === 'object') {
+        this.setState({
+          showSignUp: false,
+          email: response.data.email,
+          showWelcome: true,
+          isLoggedIn: true
+        })
+        localStorage.setItem('UserName', response.data.email);
+        localStorage.setItem('Token', response.data.token);
+        localStorage.setItem('UserId', response.data.id)
+      }
+      else if (typeof response.data === 'string') {
+        this.setState({
+          errorMessage: response.data,
+          showError: true
+        })
+      }
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }
+
+  /** Sets email for submission
+    * @param={event}
+   **/
+  setEmail = (event) => {
+    this.setState({
+      email: event.target.value
+    })
+  }
+
+  /** Sets password for submission of sign up
+    * @param={event}
+   **/
+  setPassword = (event) => {
+    this.setState({
+      password: event.target.value
+    })
+  }
+
   /**
   * @return {void}
   */
@@ -48,8 +114,19 @@ class NavBar extends Component {
     return (
       <div className="navDiv">
         {this.state.showWelcome ? <p>Welcome {this.state.email}</p> : ''}
-        {this.state.isLoggedIn ? <LogOut />: <button onClick={this.showSignUp}>Sign Up</button>}
-        {this.state.showSignUp ? <SignUp />: ('')}
+        {this.state.isLoggedIn ?  <button onClick={this.logOut}>Log Out</button> :
+          <button onClick={this.showSignUp}>Sign Up</button>}
+        {this.state.showSignUp ?
+           <div>
+              <label>Email</label>
+              <input type="text" value={this.state.email} onChange={this.setEmail}/>
+              <label>Password</label>
+              <input type="password" value={this.state.password}
+                onChange={this.setPassword}/>
+              <input type="submit" value="Submit" onClick={this.submitSignUp}/>
+                {this.state.showError ? <p>{this.state.errorMessage}</p> : ''}
+              </div>
+                : ('')}
       </div>
     );
   }
